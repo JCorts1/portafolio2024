b15c216cbbec0c63e895765572b0d82069d53c89
Jay Cortes <cortesjay1991@gmail.com>
2024-07-31 12:58:15 +0200

    Model and controller are present no errors yet

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,33 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: [:show]
+
+  def index
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+  end
+
+  def show
+    # Before actions is present
+  end
+
+  def update
+    @message.update(message_params)
+  end
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:name, :email, :content)
+  end
+end

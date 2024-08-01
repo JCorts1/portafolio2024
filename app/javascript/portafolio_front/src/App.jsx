73f455ea6574da7adc809b968a7cb17dd9c79b31
Jay Cortes <cortesjay1991@gmail.com>
2024-08-01 13:31:03 +0200

    first Home views

--- app/javascript/portafolio_front/src/App.jsx
+++ app/javascript/portafolio_front/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import Messages from './components/Messages'
+import Home from './views/Home'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className='w-100 h-screen bg-black flex justify-center content-center'>
-      <h1 className='text-lime-500 justify-center content-center'>Portafolio in Progress!</h1>
-      <Messages />
+    <div>
+      <Home/>
     </div>
   )
 }

64bcb55b21b9d607f9a4915e641d6686386265f1
Jay Cortes <cortesjay1991@gmail.com>
2024-08-15 11:35:53 +0200

    React Router Dom has been implemented

--- app/javascript/portafolio_front/src/App.jsx
+++ app/javascript/portafolio_front/src/App.jsx
@@ -1,13 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import Messages from './components/Messages'
 import Home from './views/Home'
+
 import './App.css'
 
 function App() {
 
   return (
     <div>
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }

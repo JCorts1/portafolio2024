ce7845e8037364e1af63726dc680ca223be2ee7a
Jay Cortes <cortesjay1991@gmail.com>
2024-08-17 19:03:33 +0200

    projects is working now. However Contact backend is not working anymore. Looks like a problem from front end

--- app/javascript/portafolio_front/src/views/Projects.jsx
+++ app/javascript/portafolio_front/src/views/Projects.jsx
@@ -5,7 +5,14 @@ import { CarouselOrientation } from "@/components/CarouselOrientation"
 const Projects = () => {
   return (
     <div className='projects w-screen h-screen flex justify-center items-center'>
-      <CarouselOrientation/>
+      <div className="projects-filter w-screen h-screen flex items-center">
+        <div className="local-loop w-5/12 h-5/6 border-2 rounded-lg flex justify-between items-center">
+          <h2 className="text-white">Local Loop</h2>
+          <CarouselOrientation/>
+        </div>
+
+      </div>
+
     </div>
   )
 }

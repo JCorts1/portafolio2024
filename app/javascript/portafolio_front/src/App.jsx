68320a4a7d93a5096fe9bd283c4cfc46f9f422fb
Jay Cortes <cortesjay1991@gmail.com>
2024-08-17 14:28:25 +0200

    Project rout has been stablished

--- app/javascript/portafolio_front/src/App.jsx
+++ app/javascript/portafolio_front/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './views/Home'
 import Contact from './views/Contact'
+import Projects from './views/Projects'
 
 import './App.css'
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/contact' element={<Contact/>} />
+          <Route path='/projects' element={<Projects/>} />
         </Routes>
       </BrowserRouter>
     </div>

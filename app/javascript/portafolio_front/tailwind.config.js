853ea127a8c6eab84c1a8d1567728f2ecfa408a7
Jay Cortes <cortesjay1991@gmail.com>
2024-08-18 10:41:12 +0200

    fonts bug has been solved

--- app/javascript/portafolio_front/tailwind.config.js
+++ app/javascript/portafolio_front/tailwind.config.js
@@ -71,6 +71,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        sans: ['Roboto Condensed'], // Add Roboto Condensed to the sans-serif stack
+        Sedgwick: ['Sedgwick Ave'], // Create a custom font family for Sedgwick Ave
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],

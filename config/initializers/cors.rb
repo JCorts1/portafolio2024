5fa26c362ea983cb2ce88ad43471129cd4e9039a
Jay Cortes <cortesjay1991@gmail.com>
2024-08-28 15:39:12 +0200

    fetch front and back and are not connected

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins "http://localhost:7030"
+     origins ENV.fetch["URL_FRONT"] || "http://localhost:7030"
 
      resource "*",
        headers: :any,

c2c5922aa2f2968a7c5601daed137114622f4414
Jay Cortes <cortesjay1991@gmail.com>
2024-08-28 15:47:03 +0200

    cors changes

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins ENV.fetch["URL_FRONT"] || "http://localhost:7030"
+     origins ENV["URL_FRONT"] || "http://localhost:7030"
 
      resource "*",
        headers: :any,

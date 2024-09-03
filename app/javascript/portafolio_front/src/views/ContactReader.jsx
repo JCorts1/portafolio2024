440b153157f2c43abf08c83dd2e1a815731bd006
Jay Cortes <cortesjay1991@gmail.com>
2024-09-03 16:35:07 +0200

    contact_reader is not displaying front-end

---
+++ app/javascript/portafolio_front/src/views/ContactReader.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import {useState, useEffect } from "react";
+import Navbar from '@/components/Navbar';
+import axios from "axios";
+
+const ContactReader = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://13.51.37.112:3000/messages").then(function (response) {
+      setMessages(response.data);
+      console.log(response);
+    });
+  }, []);
+
+  <div className='bg-[#000] w-screen h-screen'>
+    <div className='bg-black'>
+      < Navbar />
+    </div>
+    <div className='w-10/12 h-full bg-black'>
+      {messages.map((message) => {
+        return (
+          <div
+            key={message.id}
+            className='message-card'
+          >
+          <div className=''>
+            <h1>{message.name}</h1>
+            <h3>{message.email}</h3>
+            <p>{message.content}</p>
+          </div>
+          </div>
+        );
+      })};
+
+    </div>
+  </div>
+}
+
+export default ContactReader
